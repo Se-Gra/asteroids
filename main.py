408ab29e3e72f99568a8e5396b16af72fa1e4d9e
Sebastian <sebastian@gradwohls.at>
2026-01-08 13:52:25 +0100

    start ann close of window

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 1:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+
+        # game
+
+        pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
