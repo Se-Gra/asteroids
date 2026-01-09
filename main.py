ec0fbf53fb412ed0a90ad16c833411b3afc93caa
Sebastian <sebastian@gradwohls.at>
2026-01-09 17:38:05 +0100

    added ability to shoot

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame, sys
 
 from player import Player
-from logger import log_state
+from logger import log_state, log_event
 from asteroid import Asteroids
 from asteroidfield import AsteroidField
+from shot import Shot
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, LINE_WIDTH
 
@@ -29,6 +30,9 @@ def main():
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
 
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, drawable, updatable)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -38,6 +42,13 @@ def main():
 
         # game
         updatable.update(dt)
+        #print(asteroids)
+        for asteroid_ in asteroids:
+            if asteroid_.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         for object_ in drawable:
             object_.draw(screen)
 
--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, LINE_WIDTH
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, LINE_WIDTH, PLAYER_SHOT_RADIUS, PLAYER_SHOT_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        
+        self.timer_shot_cooldown = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -40,3 +41,10 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
+
+    def shoot(self, dt):
+        shot = Shot(self.position.x, self.position.y, PLAYER_SHOT_RADIUS)
+        vect_ = pygame.Vector2(0, 1)
+        shot.velocity = vect_.rotate(self.rotation) * PLAYER_SHOT_SPEED
